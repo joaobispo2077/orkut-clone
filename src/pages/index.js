c0df4c58eafcf2aad326fa8a5120bb0f08a491fe
João Bispo <joaobispo2077@gmail.com>
2021-07-12 14:26:40 -0300

    feat: add global styles with base structure

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import Box from '../components/Box';
+
+import MainGrid from '../components/MainGrid';
+
+function ProfileSideBar() {
+  return (
+    <div className="profile" style={{ gridArea: 'profile' }}>
+      <Box><img src="https://avatars.githubusercontent.com/u/43768058?v=4" /></Box>
+    </div>
+  )
+}
+
+
+export default function Home() {
+  return (
+    <MainGrid>
+
+      <ProfileSideBar />
+
+      <div style={{ gridArea: 'feed' }}>
+        <Box>Bem vindo</Box>
+      </div>
+
+      <div style={{ gridArea: 'comunity' }}>
+        <Box>Lista amigos</Box>
+        <Box>Comunidades</Box>
+      </div>
+
+    </MainGrid>
+  )
+}
