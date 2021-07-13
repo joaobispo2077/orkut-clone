256b85df22401cec1aa2b6fdb3a7c542926ed94a
João Bispo <joaobispo2077@gmail.com>
2021-07-13 14:01:30 -0300

    feat: add form and insert communities

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
-import { OrkutMenu, OrkutNostalgicIconSet, OrkutProfileSidebarMenuDefault } from '../lib/OrkutCommons';
-import Box from '../components/Box';
+import { useState } from 'react';
 
+import { OrkutMenu, OrkutNostalgicIconSet, OrkutProfileSidebarMenuDefault } from '../lib/OrkutCommons';
+
+import Box from '../components/Box';
 import MainGrid from '../components/MainGrid';
 import { ProfileRelationsBoxWrapper } from '../components/ProfileRelations';
 
@@ -36,6 +38,20 @@ export default function Home() {
     'Viviane-Queiroz'
   ];
 
+  const [communities, setCommunities] = useState([]);
+  const [communityName, setCommunityName] = useState('');
+  const [communityURL, setCommunityURL] = useState('');
+
+  const handleCreateCommunity = (event) => {
+    event.preventDefault();
+
+    if (communityName.length > 0 && communityURL.length > 0) {
+      setCommunities((prevCommunities) => prevCommunities.concat({ name: communityName, url: communityURL }));
+      setCommunityName('');
+      setCommunityURL('');
+    }
+  }
+
   return (
     <>
       <OrkutMenu />
@@ -51,6 +67,37 @@ export default function Home() {
             </h1>
 
             <OrkutNostalgicIconSet />
+
+          </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleCreateCommunity}>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Qual vai ser o nome da sua comuidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  value={communityName}
+                  onChange={(event) => setCommunityName(event.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                  value={communityURL}
+                  onChange={(event) => setCommunityURL(event.target.value)}
+                />
+              </div>
+
+              <button>
+                Criar comunidade
+              </button>
+            </form>
           </Box>
         </div>
 
@@ -71,7 +118,26 @@ export default function Home() {
               ))}
             </ul>
           </ProfileRelationsBoxWrapper>
-          {/* <Box>Comunidades</Box> */}
+
+          {communities.length > 0 && (
+            <ProfileRelationsBoxWrapper>
+              <h2 className="smallTitle">
+                Comunidades
+              </h2>
+
+              <ul>
+                {communities.map((community, index) => (
+                  <li key={community.name}>
+                    <a>
+                      <img src={community.url} alt={community.name} />
+                      <span>{community.name}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </ProfileRelationsBoxWrapper>
+          )}
+
         </div>
 
       </MainGrid>
