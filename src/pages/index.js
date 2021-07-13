c3bca76c725db3fc4f09387bb3d243f72e469c36
João Bispo <joaobispo2077@gmail.com>
2021-07-12 21:24:13 -0300

    feat: add favorite persons and feed icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,64 @@
-import { OrkutMenu } from '../lib/OrkutCommons';
+import { OrkutMenu, OrkutNostalgicIconSet } from '../lib/OrkutCommons';
 import Box from '../components/Box';
 
 import MainGrid from '../components/MainGrid';
+import { ProfileRelationsBoxWrapper } from '../components/ProfileRelations';
 
-function ProfileSideBar() {
+function ProfileSideBar({ githubUsername }) {
   return (
     <div className="profile" style={{ gridArea: 'profile' }}>
-      <Box><img src="https://avatars.githubusercontent.com/u/43768058?v=4" /></Box>
+      <Box><img src={`https://github.com/${githubUsername}.png`} /></Box>
     </div>
   )
 }
 
 export default function Home() {
+  const githubUsername = 'joaobispo2077';
+  const favoritePersons = [
+    'Viviane-Queiroz',
+    'Viviane-Queiroz',
+    'Viviane-Queiroz',
+    'Viviane-Queiroz',
+    'Viviane-Queiroz',
+    'Viviane-Queiroz'
+  ];
+
   return (
     <>
       <OrkutMenu />
       <MainGrid>
-
-        <ProfileSideBar />
+        <div className="profile">
+          <ProfileSideBar githubUsername={githubUsername} />
+        </div>
 
         <div style={{ gridArea: 'feed' }}>
-          <Box>Bem vindo</Box>
+          <Box>
+            <h1 className="title">
+              Bem vindo (a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
         </div>
 
         <div style={{ gridArea: 'comunity' }}>
-          <Box>Lista amigos</Box>
-          <Box>Comunidades</Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade - {favoritePersons.length}
+            </h2>
+
+            <ul>
+              {favoritePersons.map((favoritePerson, index) => (
+                <li key={favoritePerson + index}>
+                  <a href={`/users/${favoritePerson}`}>
+                    <img src={`https://github.com/${favoritePerson}.png`} alt={favoritePerson} />
+                    <span>{favoritePerson}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          {/* <Box>Comunidades</Box> */}
         </div>
 
       </MainGrid>
